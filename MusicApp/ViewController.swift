6f8c88fae66e96f297a412853492ff230578fc11
muhammed dursun <mamiankara10@gmail.com>
2024-05-02 02:30:12 +0300

    The project has been started.

---
+++ MusicApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by muhammed dursun on 2.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
