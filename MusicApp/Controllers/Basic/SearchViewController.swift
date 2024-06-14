138e222d303b48a581ed2ce0a23c4efad91a4ea3
muhammed dursun <mamiankara10@gmail.com>
2024-06-14 09:12:44 +0300

    A structure has been established where we can search on the search screen. Additionally, the interface of the species that will be listed before searching on the Search screen has been created.

--- MusicApp/Controllers/Basic/SearchViewController.swift
+++ MusicApp/Controllers/Basic/SearchViewController.swift
@@ -9,11 +9,92 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    // MARK: - Properties
+    
+    let searchController : UISearchController = {
+        let sc = UISearchController(searchResultsController: SearchResultsViewController())
+        sc.searchBar.placeholder = "Albums,Artists,Songs.."
+        sc.searchBar.searchBarStyle = .minimal
+        sc.definesPresentationContext = true
+        return sc
+    }()
+    private let collectionView : UICollectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: UICollectionViewCompositionalLayout(sectionProvider: { _,_  -> NSCollectionLayoutSection? in
+            let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                                                 heightDimension: .fractionalHeight(1.0)))
+            item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 6, bottom: 2, trailing: 6)
+            let horizontalGroup = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(160)),
+                                                                     subitem: item,
+                                                                     count:2
+            )
+            horizontalGroup.contentInsets = NSDirectionalEdgeInsets(top: 9, leading: 0, bottom: 9, trailing: 0)
+            return NSCollectionLayoutSection(group: horizontalGroup)
+        })
+        
+    )
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.backgroundColor = .systemBackground
+        searchController.searchResultsUpdater = self
+        navigationItem.searchController = searchController
+        view.addSubview(collectionView)
+        collectionView.register(GenreCollectionViewCell.self, forCellWithReuseIdentifier: GenreCollectionViewCell.identifer)
+        collectionView.backgroundColor = .systemBackground
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        collectionView.frame = view.bounds
+    }
+    
+}
 
+// MARK: - UISearchResultsUpdating
+
+extension SearchViewController : UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        
+        guard let resultsController = searchController.searchResultsController as? SearchResultsViewController,
+              let queryText =  searchController.searchBar.text, !queryText.trimmingCharacters(in: .whitespaces).isEmpty
+        else {return}
+        
+        print(queryText)
+        
+    }
+    
+    
+}
+
+// MARK: - UICollectionViewDelegate,UICollectionViewDataSource
+
+extension SearchViewController : UICollectionViewDelegate,UICollectionViewDataSource{
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+       
+       guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: GenreCollectionViewCell.identifer,
+                                                           for: indexPath) as? GenreCollectionViewCell
+        else{return UICollectionViewCell()}
+       // cell.backgroundColor = .systemBrown
+        cell.configure(title: "Slow")
+        return cell
+    }
+   
 }
--- MusicApp/Views/GenreCollectionViewCell.swift
+++ MusicApp/Views/GenreCollectionViewCell.swift
@@ -5,4 +5,72 @@
 //  Created by muhammed dursun on 14.06.2024.
 //
 
-import Foundation
+import UIKit
+
+class GenreCollectionViewCell : UICollectionViewCell {
+    
+    // MARK: - Properties
+    
+    static let identifer = "GenreCollectionViewCell"
+    private let imageView : UIImageView = {
+       let image = UIImageView()
+        image.contentMode = .scaleAspectFit
+        image.tintColor = .white
+        image.image = UIImage(systemName: "music.quarternote.3",
+                              withConfiguration: UIImage.SymbolConfiguration(pointSize: 50, weight: .regular))
+        return image
+    }()
+    private let label : UILabel = {
+        let label  = UILabel()
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 21, weight: .semibold)
+        return label
+    }()
+    private var colors: [UIColor] = [
+        .systemPink,
+        .systemPurple,
+        .systemOrange,
+        .systemBlue,
+        .systemBrown,
+        .systemYellow,
+        .systemGray,
+        .systemGreen
+    ]
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        contentView.layer.cornerRadius = 7
+        contentView.layer.masksToBounds = true
+        contentView.addSubview(imageView)
+        contentView.addSubview(label)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        label.frame = CGRect(x: 10, y: contentView.height/2, width: contentView.width - 20 , height: contentView.height / 2)
+        imageView.frame  = CGRect(x: contentView.width/2, y: 0, width: contentView.width/2, height: contentView.height/2)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        label.text = nil
+    }
+    
+    // MARK: - Assistants
+    
+    func configure(title : String){
+        
+        label.text = title
+        contentView.backgroundColor = colors.randomElement()
+    }
+    
+}
